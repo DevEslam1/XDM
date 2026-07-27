dea480e58d056e87311987cb46f6baa9d048c6a3
Eslam <xdev.eslam@gmail.com>
2026-07-28 02:07:54 +0300

    refactor: relocate test scripts to the test directory

--- test/test_playlist.dart
+++ test/test_playlist.dart
@@ -1,33 +1,3 @@
 // ignore_for_file: avoid_print
-// Test that the YoutubeService methods work with the fallback
-import 'package:dmx/core/services/youtube_service.dart';
-import 'package:logging/logging.dart';
+import 'package:dmx/core/services/youtube_service.dart'; void main() async { final details = await YoutubeService.getPlaylistDetails('https://www.youtube.com/playlist?list=PLFgofPyD3Bck5vCNEsD9SxpIa74x3a1h2'); print('Videos count: ${details?["videos"]?.length}'); }
 
-void main() async {
-  Logger.root.level = Level.ALL;
-  Logger.root.onRecord.listen((e) {
-    print('[${e.level.name}] ${e.loggerName}: ${e.message}');
-    if (e.error != null) {
-      print(e.error);
-    }
-  });
-
-  final url = 'https://www.youtube.com/playlist?list=PLDoPjvoNmBAy532K9M_fjiAmrJ0gkCyLJ';
-
-  print('=== YoutubeService.getPlaylistVideos ===');
-  final videos = await YoutubeService.getPlaylistVideos(url);
-  if (videos != null) {
-    print('Total videos: ${videos.length}');
-    for (var i = 0; i < videos.length && i < 5; i++) {
-      final v = videos[i];
-      print('  ${i + 1}. [${v['id']}] "${v['title']}" - ${v['duration']}s');
-    }
-    if (videos.length > 5) {
-      print('  ... and ${videos.length - 5} more');
-    }
-  } else {
-    print('No videos found.');
-  }
-
-  YoutubeService.close();
-}
